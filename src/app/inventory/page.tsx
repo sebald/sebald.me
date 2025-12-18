a1757067c9459eb0b3469eab1f700795e1115dad
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-12-18 09:19:57 +0100

    save

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -2,6 +2,7 @@ const InventoryPage = () => {
   return (
     <div className="grid justify-items-center gap-10">
       <div className="bg-glass-surface shadow-glass border-glass-stroke aspect-video w-1/3 rounded-xl border backdrop-filter-(--backdrop-glass)"></div>
+      <div className="bg-glass-dark-surface shadow-glass-dark border-glass-dark-stroke aspect-video w-1/3 rounded-xl border backdrop-filter-(--backdrop-glass)"></div>
     </div>
   );
 };
