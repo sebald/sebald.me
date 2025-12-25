0b293fc69903192a9936070e872158677fc797a0
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-12-25 08:19:02 +0100

    adjust buttons a bit

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -51,7 +51,7 @@ export const styles = cva({
         '[--btn-border-hover:linear-gradient(to_bottom,var(--color-blueberry-300),var(--color-blueberry-600))]',
         'shadow-[inset_0_1px_0_0_oklch(1_0_0/0.4),inset_0_-2px_4px_0_oklch(0_0_0/0.2),0_4px_12px_-2px_oklch(0_0_0/0.4),0_2px_4px_-1px_oklch(0_0_0/0.1)]',
       ],
-      ghost: ['text-current', 'bg-oatmeal-500/25', 'hover:bg-oatmeal-500/15'],
+      ghost: ['text-current', 'bg-oatmeal-500/20', 'hover:bg-oatmeal-500/10'],
       outline: [
         'text-current',
         'border-[oklch(from_currentColor_l_c_h/0.4)]',
