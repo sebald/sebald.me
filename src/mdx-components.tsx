b056d00598604872782f25de807afbca9d867bf1
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-01 07:51:46 +0100

    save

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,7 @@ import type { MDXComponents } from 'mdx/types';
 import { Blockquote } from '@/ui/blockquote';
 import { CodeBlock, CodeBlockTabs, CodeBlockTabsList } from '@/ui/codeblock';
 import { Headline } from '@/ui/headline';
+import { Image } from '@/ui/image';
 import { Link } from '@/ui/link';
 
 export const getMDXComponents = (
@@ -17,35 +18,40 @@ export const getMDXComponents = (
     </Blockquote>
   ),
   h1: ({ children, ...props }) => (
-    <Headline level="1" as="h1" {...props}>
+    <Headline level="1" {...props}>
       {children}
     </Headline>
   ),
   h2: ({ children, ...props }) => (
-    <Headline level="2" as="h2" {...props}>
+    <Headline level="2" {...props}>
       {children}
     </Headline>
   ),
   h3: ({ children, ...props }) => (
-    <Headline level="3" as="h3" {...props}>
+    <Headline level="3" {...props}>
       {children}
     </Headline>
   ),
   h4: ({ children, ...props }) => (
-    <Headline level="4" as="h4" {...props}>
+    <Headline level="4" {...props}>
       {children}
     </Headline>
   ),
   h5: ({ children, ...props }) => (
-    <Headline level="5" as="h5" {...props}>
+    <Headline level="5" {...props}>
       {children}
     </Headline>
   ),
   h6: ({ children, ...props }) => (
-    <Headline level="6" as="h6" {...props}>
+    <Headline level="6" {...props}>
       {children}
     </Headline>
   ),
+
+  // Support captions
+  img: Image,
+
+  // Override to adjust styling
   pre: ({ children, ...props }) => <CodeBlock {...props}>{children}</CodeBlock>,
   CodeBlockTabs,
   CodeBlockTabsList,
