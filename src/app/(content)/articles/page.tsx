397284c916b15d3e56c86d1eef4b08c3d8e6f25e
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-01 18:42:40 +0100

    style: make page title smaller

--- src/app/(content)/articles/page.tsx
+++ src/app/(content)/articles/page.tsx
@@ -5,11 +5,6 @@ import { articlesSource, getExcerpt, sortByDate } from '@/lib/source';
 import { Article } from '@/ui/article';
 import { Headline } from '@/ui/headline';
 import { Link } from '@/ui/link';
-import { Text } from '@/ui/text';
-
-// Config
-// ---------------
-export const revalidate = false;
 
 // Meta
 // ---------------
@@ -24,43 +19,39 @@ const ArticlesIndex = async () => {
   const articles = sortByDate(articlesSource.getPages());
 
   return (
-    <div className="mx-auto grid max-w-[75ch] gap-20">
-      <div>
-        <Headline level="1" variant="accent" as="h1">
-          Articles
-        </Headline>
-        <Text variant="faded" size="lead" leading="inherit">
-          Notes from the road and lessons learned along the way. Welcome to the
-          archive.
-        </Text>
-      </div>
+    <div className="mx-auto grid max-w-[75ch] gap-6">
+      <Headline level="5" variant="accent" as="h1">
+        Articles
+      </Headline>
 
-      {articles.map(async (page) => {
-        const excerpt = await getExcerpt(page);
-        return (
-          <Article key={page.url} aria-labelledby={page.url} space="2">
-            <Article.Header flow="reverse">
-              <Article.Title id={page.url} variant="list">
-                {page.data.title}
-              </Article.Title>
-              <Article.Meta date={page.data.date} topics={page.data.topics} />
-            </Article.Header>
-            <Article.Excerpt>{excerpt}</Article.Excerpt>
-            <Link
-              href={page.url}
-              aria-label={`Read article: ${page.data.title}`}
-              noUnderline
-            >
-              Read full article
-              <ArrowRightIcon
-                className="transition-transform duration-200 group-hover/link:translate-x-1"
-                size={16}
-                weight="bold"
-              />
-            </Link>
-          </Article>
-        );
-      })}
+      <div className="space-y-20">
+        {articles.map(async (page) => {
+          const excerpt = await getExcerpt(page);
+          return (
+            <Article key={page.url} aria-labelledby={page.url} space="2">
+              <Article.Header flow="reverse">
+                <Article.Title id={page.url} variant="list">
+                  {page.data.title}
+                </Article.Title>
+                <Article.Meta date={page.data.date} topics={page.data.topics} />
+              </Article.Header>
+              <Article.Excerpt>{excerpt}</Article.Excerpt>
+              <Link
+                href={page.url}
+                aria-label={`Read article: ${page.data.title}`}
+                noUnderline
+              >
+                Read full article
+                <ArrowRightIcon
+                  className="transition-transform duration-200 group-hover/link:translate-x-1"
+                  size={16}
+                  weight="bold"
+                />
+              </Link>
+            </Article>
+          );
+        })}
+      </div>
     </div>
   );
 };
