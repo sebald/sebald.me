967aadeb365788e3991d8a125ce774bda1cc014b
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-06 00:13:41 +0100

    save

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -1,7 +1,10 @@
 import {
   CopyrightIcon,
+  CubeIcon,
   MarkdownLogoIcon,
+  RssIcon,
 } from '@phosphor-icons/react/dist/ssr';
+import type { PropsWithChildren } from 'react';
 
 import { socialLinks } from '@/app.config';
 import { ConsentUpdate } from '@/ui/analytics/consent-update';
@@ -11,67 +14,104 @@ import { LinkedInIcon } from '@/ui/icon/linkedin-icon';
 import { XComIcon } from '@/ui/icon/x-com-icon';
 import { Link } from '@/ui/link';
 
+const Legal = () => (
+  <div className="flex items-center gap-2">
+    <Link variant="inherit" noUnderline href="/imprint">
+      Imprint
+    </Link>
+    <span className="text-muted text-sm">·</span>
+    <Link variant="inherit" noUnderline href="/privacy">
+      Privacy Policy
+    </Link>
+    <span className="text-muted text-sm">·</span>
+    <ConsentUpdate />
+  </div>
+);
+
+const Social = () => (
+  <div className="flex items-center gap-8">
+    <Link
+      aria-label="Me on X GitHub"
+      variant="inherit"
+      noUnderline
+      href={socialLinks.github}
+      target="_blank"
+    >
+      <GithubIcon size={20} />
+    </Link>
+    <Link
+      aria-label="Me on LinkedIn"
+      variant="inherit"
+      noUnderline
+      href={socialLinks.linkedin}
+      target="_blank"
+    >
+      <LinkedInIcon size={20} />
+    </Link>
+    <Link
+      aria-label="Me on X (formerly Twitter)"
+      variant="inherit"
+      noUnderline
+      href={socialLinks.x}
+      target="_blank"
+    >
+      <XComIcon size={20} />
+    </Link>
+  </div>
+);
+
+const Links = () => (
+  <div className="flex items-center gap-6">
+    <Link
+      aria-label="View all content formatted for LLMs"
+      variant="inherit"
+      noUnderline
+      href="/llms.txt"
+    >
+      <MarkdownLogoIcon size={16} />
+      llms.txt
+    </Link>
+    <Link
+      aria-label="View RSS feed"
+      variant="inherit"
+      noUnderline
+      href="/rss.xml"
+    >
+      <RssIcon size={16} />
+      RSS Feed
+    </Link>
+    <Link
+      aria-label="View inventory"
+      variant="inherit"
+      noUnderline
+      href="/inventory"
+    >
+      <CubeIcon size={16} />
+      Inventory
+    </Link>
+  </div>
+);
+
+const Copyright = () => (
+  <div className="flex items-center justify-center gap-1 md:justify-start">
+    <CopyrightIcon size={14} />
+    {new Date().getFullYear()} Sebastian Sebald
+  </div>
+);
+
+// Component
+// ---------------
 export const Footer = () => (
   <div className="fit-prose pb-4 pt-28 md:pb-6">
     <Divider variant="light" />
-    <div className="text-muted grid gap-10 pt-6 text-sm md:gap-2">
-      {/* Line 1 */}
-      <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-        <div className="flex items-center gap-2">
-          <Link variant="inherit" noUnderline href="/imprint">
-            Imprint
-          </Link>
-          <span className="text-muted text-sm">·</span>
-          <Link variant="inherit" noUnderline href="/privacy">
-            Privacy Policy
-          </Link>
-          <span className="text-muted text-sm">·</span>
-          <ConsentUpdate />
-        </div>
-
-        <div className="flex items-center gap-8">
-          <Link
-            aria-label="Me on X GitHub"
-            variant="inherit"
-            noUnderline
-            href={socialLinks.github}
-            target="_blank"
-          >
-            <GithubIcon size={16} />
-          </Link>
-          <Link
-            aria-label="Me on LinkedIn"
-            variant="inherit"
-            noUnderline
-            href={socialLinks.linkedin}
-            target="_blank"
-          >
-            <LinkedInIcon size={16} />
-          </Link>
-          <Link
-            aria-label="Me on X (formerly Twitter)"
-            variant="inherit"
-            noUnderline
-            href={socialLinks.x}
-            target="_blank"
-          >
-            <XComIcon size={16} />
-          </Link>
-          <Link
-            aria-label="Me on X GitHub"
-            variant="inherit"
-            noUnderline
-            href="/llms.txt"
-          >
-            <MarkdownLogoIcon weight="regular" size={20} />
-          </Link>
-        </div>
+    <div className="text-muted grid gap-10 pt-6 text-sm">
+      <div className="flex justify-between">
+        <Links />
+        <Social />
       </div>
-
-      {/* Line 2 */}
-      <div className="flex flex-1 items-center justify-center gap-1 md:justify-start">
-        <CopyrightIcon size={14} />
-        {new Date().getFullYear()} Sebastian Sebald
+      <div className="flex justify-between">
+        <Legal />
+        <Copyright />
       </div>
     </div>
   </div>
