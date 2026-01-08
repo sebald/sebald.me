a0dba303e06dba9b64b5b30de6412f1f8c41eeba
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-08 18:01:55 +0100

    save

--- src/app/(content)/articles/page.tsx
+++ src/app/(content)/articles/page.tsx
@@ -27,10 +27,10 @@ const ArticlesPage = async () => {
   return (
     <div className="fit-prose grid gap-12 md:gap-16">
       <div className="flex items-end justify-between">
-        <Headline level="3" variant="accent" as="h1">
+        <Headline level="4" variant="muted" as="h1">
           {page.title}
         </Headline>
-        <div className="text-muted flex items-center gap-2 text-sm">
+        <div className="text-muted flex gap-2 text-sm">
           <Link
             aria-label="View articles as markdown"
             variant="inherit"
