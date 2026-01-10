685e2902145ad72fe9563dda94752e90b8555b3e
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-10 16:22:09 +0100

    save

---
+++ src/app/(page)/about/intro.tsx
@@ -0,0 +1,65 @@
+import { ParallaxImage } from '@/ui/parallax-image';
+
+const layers = [
+  {
+    id: 'background',
+    src: '/0-background.webp',
+    width: 1000,
+    height: 1000,
+    config: {
+      width: '400px',
+      xMove: '-10px',
+      yMove: '-10px',
+    },
+  },
+  {
+    id: 'window',
+    src: '/1-window.webp',
+    width: 1000,
+    height: 1000,
+    config: {
+      width: '400px',
+      xMove: '-15px',
+      yMove: '-10px',
+    },
+  },
+  {
+    id: 'person',
+    src: '/2-person.webp',
+    width: 1000,
+    height: 1000,
+    // slightly move to center the person
+    className: 'left-[56%]',
+    config: {
+      width: '450px',
+      xMove: '-30px',
+      yMove: '-25px',
+    },
+  },
+];
+
+export const Intro = () => (
+  <div className="prose flow-root">
+    <ParallaxImage
+      layers={layers}
+      width="350px"
+      aspect="5/6"
+      className="float-left mb-6 mr-12 rounded-3xl"
+    />
+    <p className="mt-0 font-sans text-4xl font-bold">
+      Hi there! My name is Sebastian.
+    </p>
+    <p>
+      I am a software architect at Reservix located in Freiburg, Germany. My
+      focus is design engineering, allowing me to operate right in the middle
+      where code meets user experience. I create digital products that are
+      accessible, intuitive, and delightful to use.
+    </p>
+    <p>
+      Design Systems are at the core of my work. To me, design sytems are much
+      more than just a UI kit or component library. They are the infrastructure
+      that helps teams ship great products smoothly at scale. The services and
+      processes within them help bridge the gap between design vision and code.
+    </p>
+  </div>
+);
--- src/app/(page)/about/page.tsx
+++ src/app/(page)/about/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 import { Headline } from '@/ui/headline';
 
-import { Avatar } from './avatar';
+import { Intro } from './intro';
 
 // Config
 // ---------------
@@ -23,32 +23,7 @@ const AboutPage = () => {
       <Headline level="2" variant="accent" as="h1">
         About
       </Headline>
-      <div className="grid gap-8">
-        <div className="grid grid-cols-[max-content_1fr] gap-12">
-          <Avatar />
-          <div className="prose place-self-end">
-            <p className="font-sans text-4xl font-bold">
-              Hey there! My name is Sebastian.
-            </p>
-            <p>
-              I am a software architect at Reservix located in Freiburg,
-              Germany. My focus is design engineering, allowing me to operate
-              right in the middle where code meets user experience. I create
-              digital products that are accessible, intuitive, and delightful to
-              use.
-            </p>
-          </div>
-        </div>
-        <div className="prose">
-          <p>
-            Design Systems are at the core of my work. To me, design sytems are
-            much more than just a UI kit or component library. They are the
-            infrastructure that helps teams ship great products smoothly at
-            scale. The services and processes within them help bridge the gap
-            between design vision and code.
-          </p>
-        </div>
-      </div>
+      <Intro />
     </div>
   );
 };
