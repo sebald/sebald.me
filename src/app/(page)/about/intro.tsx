0c86ad298fc5efcca26c573bc9a61f842835ebce
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-11 07:39:57 +0100

    update

--- src/app/(page)/about/intro.tsx
+++ src/app/(page)/about/intro.tsx
@@ -46,21 +46,21 @@ export const Intro = () => (
       aspect="5/6"
       className="mx-auto mb-12 rounded-3xl md:float-left md:mb-6 md:mr-12"
     />
-    <p className="mt-0 font-sans text-4xl font-bold">
-      Hi there! My name is Sebastian.
+    <p className="mt-0 font-sans text-4xl font-bold" aria-hidden="true">
+      Hi there!
     </p>
     <p>
-      I am a software architect at Reservix located in Freiburg, Germany. My
-      focus is design engineering, allowing me to operate right in the middle
-      where code meets user experience. I create digital products that are
+      I am Sebastian, a software architect at Reservix based in Freiburg,
+      Germany. I specialize in design engineering, working right where code
+      meets user experience. My goal is to create digital products that are
       accessible, intuitive, and delightful to use.
     </p>
     <p>
-      Design Systems are at the core of my work. To me, design sytems are much
-      more than just a UI kit or component library. They are the infrastructure
-      that help organizations ship great products smoothly at scale. The
-      services and processes within them help bridge the gap between design
-      vision and code.
+      Design systems are at the core of my work. To me, they are much more than
+      just a UI kit or component library. They serve as the infrastructure that
+      empowers organizations to ship great products smoothly, providing the
+      framework needed to translate creative vision into technical reality at
+      scale.
     </p>
   </div>
 );
